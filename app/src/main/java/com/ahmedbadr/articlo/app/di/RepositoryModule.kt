c7c4e1e5c1d69ec142a92c6f819426e44e520f53
AhmedBadr <ahmed.badr.abdeldayem.dev@gmail.com>
2023-07-30 17:11:14 +0300

    add: TopicRepository to RepositoryModule.kt

--- app/src/main/java/com/ahmedbadr/articlo/app/di/RepositoryModule.kt
+++ app/src/main/java/com/ahmedbadr/articlo/app/di/RepositoryModule.kt
@@ -3,8 +3,10 @@ package com.ahmedbadr.articlo.app.di
 
 import com.ahmedbadr.articlo.data.repository.authentication.AuthenticationRepositoryImpl
 import com.ahmedbadr.articlo.data.repository.country.CountryRepositoryImpl
+import com.ahmedbadr.articlo.data.repository.topic.TopicRepositoryImpl
 import com.ahmedbadr.articlo.domain.repository.AuthenticationRepository
 import com.ahmedbadr.articlo.domain.repository.CountryRepository
+import com.ahmedbadr.articlo.domain.repository.TopicRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -12,7 +14,9 @@ import org.koin.dsl.module
 val repositoryModule = module {
     singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>() }
 
-
     // country
     singleOf(::CountryRepositoryImpl) { bind<CountryRepository>() }
+
+    // topic
+    singleOf(::TopicRepositoryImpl) { bind<TopicRepository>() }
 }
