63dbe5db1e8e79a7f25eeac602b666d2c0894044
AhmedBadr <ahmed.badr.abdeldayem.dev@gmail.com>
2023-07-30 17:10:52 +0300

    add: TopicMapper.kt TopicEntity.asTopic & TopicResponse.asTopicEntity extension functions

--- app/src/main/java/com/ahmedbadr/articlo/data/mappers/TopicMapper.kt
+++ app/src/main/java/com/ahmedbadr/articlo/data/mappers/TopicMapper.kt
@@ -6,4 +6,4 @@ import com.ahmedbadr.articlo.domain.model.data.Topic
 
 
 fun TopicResponse.asEntity() = TopicEntity(id, name, image, articleCount)
-fun TopicEntity.asData() = Topic(id, name, image, articleCount)
+fun TopicEntity.asTopic() = Topic(id, name, image, articleCount)
